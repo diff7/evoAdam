123fc3eeeefac04e6a1bdf20aa5303f43fbb226d
egorsv <geksut@gmail.com>
2019-10-28 13:06:13 +0300

    + torch uniform isntead of numpy ???

--- crossngover_draft.py
+++ crossngover_draft.py
@@ -1,5 +1,6 @@
 import itertools 
 from copy import copy
+import numpy as np
 
 
 
@@ -25,36 +26,54 @@ class CrossN:
     def make_combinations(self, nets):
         nets = self.make_ancestry(nets)
         parents = [n.ancestry for n in nets]
-        families = itertools.combinations_with_replacement(parents,
+        families = itertools.product(parents,repeat = 
                                            min(len(parents), 
-                                               self.n_combinations))
+                                              self.n_combinations))
         return families
     
-    def find_layers(model_state_dict, keyword = 'weight'):
+    def find_layers(self, model, keyword = 'weight'):
+        model_state_dict = model.state_dict()
         self.layers = [layer_name for layer_name in model_state_dict 
                        if keyword in layer_name]
         
     
-    def switch_weights(self, parentA, parentB):
+    def switch_weights(self, parentA, parentB, layers):
         
         # abundant copying may lead to unnessary memmory usage - need to optimize
         parentA_state_dict = parentA.state_dict()
         parentB_state_dict = parentB.state_dict()
         
         if parentA.ancestry != parentB.ancestry:
-            # p is responsible for number or True values to be replaced
+            # p is responsible for number or False values to be replaced
             p = self.p
             child_params = copy(parentA_state_dict)
             for layer in self.layers:
                 w = parentB_state_dict[layer]
-                mask =  np.random.choice([False, True], size=(w.shape), p=[1-p, p])
+                shape = w.shape
+                #mask =  np.random.choice([False, True], size=(shape), p=[1-p, p])
+                mask = torch.cuda.FloatTensor(shape).uniform_() > p
                 #switching weights
-                child_params[layer][mask]=w[mask]
+                print(layer, w[mask].shape)
+                print(layer, child_params[layer][mask].shape)
+                try:
+                    child_params[layer][mask]=w[mask]
+                except Exception as e:
+                    print('ERR:', e,layer)
             ancestry = '({}{}+{}{})'.format(str(p), parentA.ancestry ,str(1-p), parentB.ancestry)
             #HOWTO initiate a new model in any other way?
-            child = parentA
+            child = PConvUNet()
+            child.load_state_dict(child)
             child.ancestry = ancestry 
-            child.loaload_state_dict(child)
-            return child
+        else:
+            child = parentA
+            ancestry = '({}+{})'.format(parentA.ancestry, parentB.ancestry)
+            child.ancestry = ancestry 
+            
+        return child
+    
+#     def mutate(self, ):
+        
+        
+#     def sparta(self, ):
             
         
